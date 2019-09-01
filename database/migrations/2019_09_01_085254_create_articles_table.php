42d62a7a92de9a58177c8542b2bbcc02c25a0420
nour <nour.kilany88@gmail.com>
2019-09-01 16:32:11 +0200

    Add article migration

---
+++ database/migrations/2019_09_01_085254_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('subject');
+            $table->string('secondary_title');
+            $table->text('body');
+            $table->string('image');
+            $table->unsignedBigInteger('author_id');
+
+            $table->foreign('author_id')->references('id')->on('authors');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
